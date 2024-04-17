3154091b088ce0ad488fd9b1240bc3acdde91b8e
Lahiru N Ediriweera <nimeshlahiru86@gmail.com>
2024-04-17 20:53:40 +0530

    complete all add part with backend.

---
+++ Admin Panel/src/java/mypackage/Ladies.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author USER
+ */
+class Ladies {
+    
+    static List<Ladies> getAllLadies() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    Statement st;
+    void insertLadies(int id, String name, String description, String price, String amount, String picture) {
+            connectToDB();
+            String query ="INSERT INTO ladies VALUES("+id+",'"+name+"','"+description+"','"+price+"','"+amount+"','"+picture+"')";    
+            try {
+                st.executeUpdate(query);
+                System.out.println("Record inserted");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());           
+            }        
+        
+    }
+    
+    private void connectToDB() {
+        String driver= "com.mysql.jdbc.Driver";
+        String url="jdbc:mysql://localhost:3306/shop";
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, "root", "");
+            st = con.createStatement();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Gents.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    void inserKids(int id, String name, String description, String price, String amount, String picture) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void inserLadies(int id, String name, String description, String price, String amount, String picture) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

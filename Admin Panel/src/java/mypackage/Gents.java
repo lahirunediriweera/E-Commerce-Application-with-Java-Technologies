4afa2c5ad12438ec124b883660f8631fb0f1c9f0
Lahiru N Ediriweera <nimeshlahiru86@gmail.com>
2024-04-17 14:13:20 +0530

    Update Gents.java

--- Admin Panel/src/java/mypackage/Gents.java
+++ Admin Panel/src/java/mypackage/Gents.java
@@ -5,10 +5,51 @@
  */
 package mypackage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author USER
  */
 class Gents {
+    
+    static List<Gents> getAllGents() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    Statement st;
+    void insertGents(int id, String name, String description, String price, String amount, String picture) {
+            connectToDB();
+            String query ="INSERT INTO books VALUES("+id+",'"+name+"','"+description+"','"+price+"','"+amount+"','"+picture+"')";    
+            try {
+                st.executeUpdate(query);
+                System.out.println("Record inserted");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());           
+            }        
+        
+    }
+    
+    private void connectToDB() {
+        String driver= "com.mysql.jdbc.Driver";
+        String url="jdbc:mysql://localhost:3306/shop";
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, "root", "");
+            st = con.createStatement();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Gents.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    void inserGents(int id, String name, String description, String price, String amount, String picture) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
